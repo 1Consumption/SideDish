57025205afa950778b198c2aa20ca0026f8eecbd
1Consumption <hanseop95@gmail.com>
2020-11-28 12:34:55 +0900

    chore: 프로젝트 생성

---
+++ SideDish/SideDish/SideDishApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideDishApp.swift
+//  SideDish
+//
+//  Created by 신한섭 on 2020/11/28.
+//
+
+import SwiftUI
+
+@main
+struct SideDishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
