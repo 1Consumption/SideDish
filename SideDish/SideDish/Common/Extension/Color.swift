5a3763a7af603b52f7394816d04ffceda55871fb
1Consumption <hanseop95@gmail.com>
2020-12-02 23:37:15 +0900

    feat: DishViewModel 연동
    - onApear에서 ViewModel이 DishPublisher를 subcribe하도록 설정
    - ViewModel의 state에 따라 body에 표시되는 View가 바뀜.
    - 오류가 난 경우 다시 시도할 수 있도록 reload버튼 생성
    - 디렉토리 세분화

--- SideDish/SideDish/Common/Extension/Color.swift
+++ SideDish/SideDish/Common/Extension/Color.swift
@@ -10,4 +10,5 @@ import SwiftUI
 extension Color {
     static let themeColor: Color = Color("ThemeColor")
     static let backgroundColor: Color = Color("Background")
+    static let labelColor: Color = Color("Label")
 }
